a5b8cf12aadb5639cb69b9247a4f5aa69b920c8c
AHMAD FAHMI <171240000642@unisnu.ac.id>
2021-02-14 20:19:41 +0700

    commit pertama

---
+++ lib/ui/pages/pages.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:cilok_dart/shared/shared.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:supercharged/supercharged.dart';
+
+part 'general_page.dart';
---
+++ test/cilok_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:cilok_dart/cilok_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
